7ad74e06e76ec849e2c596d3836ec6c6e72aea42
Perumal <venkatesh.v.perumal@accenture.com>
2017-08-06 16:06:31 +0800

    dynamic article changes

--- server.js
+++ server.js
@@ -5,8 +5,41 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOneJson = {
+  "data":{    
+    "articleOne":{
+    "articleName":"ArticleOne",
+    "articlePubDate":"06Aug2017",
+    "articleContent":"This is the first article One Content"
+  },"articleTwo":{
+    "articleName":"ArticleTwo",
+    "articlePubDate":"07Aug2017",
+    "articleContent":"This is the first article Two Content"
+  },"articleThree":{
+    "articleName":"ArticleThree",
+    "articlePubDate":"08Aug2017",
+    "articleContent":"This is the first article Three Content"
+  }
+}
+};
+
+var articleHtmlContent = function(jsonData){
+  return `<html>
+    <h1>${jsonData.articleName}<h1>
+    <h2>${jsonData.articlePubDate}</h2>
+    <p>${jsonData.articleContent}</p>  
+    <a href="/">back</a>
+  </html>`
+};
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/:articleName', function (req, res) {
+  res.send(articleHtmlContent(articleOneJson.data[req.params.articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
@@ -21,7 +54,7 @@ app.get('/ui/madi.png', function (req, res) {
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
-var port = 80;
+var port = 8081;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
