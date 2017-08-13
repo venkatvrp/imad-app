5c8cc44a9680731fad5b8b6a7ad66de59b77187d
Perumal <venkatesh.v.perumal@accenture.com>
2017-08-13 15:38:55 +0800

    Updated for comment add

--- server.js
+++ server.js
@@ -6,44 +6,62 @@ var app = express();
 app.use(morgan('combined'));
 
 var articleOneJson = {
-  "data":{    
-    "articleOne":{
-    "articleName":"ArticleOne",
-    "articlePubDate":"06Aug2017",
-    "articleContent":"This is the first article One Content"
-  },"articleTwo":{
-    "articleName":"ArticleTwo",
-    "articlePubDate":"07Aug2017",
-    "articleContent":"This is the first article Two Content"
-  },"articleThree":{
-    "articleName":"ArticleThree",
-    "articlePubDate":"08Aug2017",
-    "articleContent":"This is the first article Three Content"
+  "data": {
+    "articleOne": {
+      "articleName": "ArticleOne",
+      "articlePubDate": "06Aug2017",
+      "articleContent": "This is the first article One Content"
+    }, "articleTwo": {
+      "articleName": "ArticleTwo",
+      "articlePubDate": "07Aug2017",
+      "articleContent": "This is the first article Two Content"
+    }, "articleThree": {
+      "articleName": "ArticleThree",
+      "articlePubDate": "08Aug2017",
+      "articleContent": "This is the first article Three Content"
+    }
   }
-}
 };
 
-var articleHtmlContent = function(jsonData){
+var articleHtmlContent = function (jsonData) {
   return `<html>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" rel="stylesheet"/>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script type="text/javascript" src="/ui/main.js"></script>
     <h1>${jsonData.articleName}<h1>
     <h2>${jsonData.articlePubDate}</h2>
     <p>${jsonData.articleContent}</p>  
-    <a href="/">back</a>
+    <input id="commentBox" type="textbox"></input>
+    <input id="updateComment" type="button" value="Post Comment"></input>
+    <p>Comments</p>
+    <span id="comments">
+      </span>    
+    <div><a href="/">back</a></div>
   </html>`
 };
-
 
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:articleName', function (req, res) {
+app.get('/article/:articleName', function (req, res) {
   res.send(articleHtmlContent(articleOneJson.data[req.params.articleName]));
 });
 
+app.get('/updatePost', function (req, res) {
+  var commentText = req.query.comment;
+  console.log('comment:: ' + commentText);
+  res.send(commentText);
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
