16cd14520227f17afe907a11512c3f9431e92b67
Perumal <venkatesh.v.perumal@accenture.com>
2017-08-13 15:38:30 +0800

    added api call to update comments

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,12 @@
-console.log('Loaded!');
+$( document ).ready(function() {
+    $("#updateComment").click(function(){
+        $.ajax({
+            method: "GET",
+            url: "/updatePost"+"?comment="+$("#commentBox").val(),
+        })
+        .done(function( msg ) {
+            $("#comments").append(msg+"</br>");            
+        });
+        $("#commentBox").val("");
+    });
+});
