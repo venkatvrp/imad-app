a3417cff577ec76c56a6b2d0e727827da8ac2803
venkatvrp <venkatvrp@users.noreply.github.com>
2017-08-22 17:19:20 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,7 +81,8 @@ app.get('/getComments', function (req, res) {
       if(err){
           res.status('500').send(err.toString());
       }else{
-          res.status('200').send(result.rows[0]);
+          console.log(result.rows[0]);
+          res.status('200').send('fetched results');
       }
   });
 });
