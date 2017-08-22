25f260992011227f16c748338531ccce5d036088
venkatvrp <venkatvrp@users.noreply.github.com>
2017-08-22 16:42:48 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.get('/article/:articleName', function (req, res) {
 app.get('/updatePost', function (req, res) {
   var commentText = req.query.comment;
   console.log('comment:: ' + commentText);
-  pool.query("insert into comments(comments,userid) values (?,?)",[commentText],2,function(err,result){
+  pool.query("insert into comments(comments,userid) values ($1,$2)",[commentText,2],function(err,result){
       if(err){
           res.status('500').send(err.toString());
       }else{
