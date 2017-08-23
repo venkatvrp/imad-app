35dc925ec7dc6a59ae216267daa47fd6fd1b9967
venkatvrp <venkatvrp@users.noreply.github.com>
2017-08-23 17:12:29 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser');
 
 var app = express();
 app.use(morgan('combined'));
-app.use(bodyParser);
+app.use(bodyParser.json());
 
 var config = {
     host:'db.imad.hasura-app.io',
