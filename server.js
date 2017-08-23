38d5c92cfd1d2c6420710e99b845b286ce5be487
venkatvrp <venkatvrp@users.noreply.github.com>
2017-08-23 12:49:00 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.post('/login', function (req, res) {
       if(err){
           res.status('500').send(err.toString());
       }else if(result.rows.length===0){
-          res.send("Login Failed.  Please try again");
+          res.status('401').send("Login Failed.  Please try again");
       }else{
           res.send("Login Success !! user authenticated");
       }
